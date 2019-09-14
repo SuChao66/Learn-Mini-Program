5971f8036e544653eaf3b7b2f4393d247b39fd8f
suchao <1311810748@qq.com>
2019-09-14 15:35:11 +0800

    弹窗和分享

--- Pages/home/home.js
+++ Pages/home/home.js
@@ -1,66 +1,67 @@
 // Pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast(){
+    wx.showToast({
+      title: '你好啊',
+      duration: 3000,     //控制停留时间
+      icon: 'loading',
+      mask: true,         //蒙版效果
+      success: function(){
+        console.log("展示弹窗成功")
+      },
+      fail:function(){
+        console.log("展示弹窗失败")
+      },
+      complete:function(){
+        console.log("完成函数的调用")
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal(){
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容',
+      //showCancel: false,
+      cancelText: "退出",
+      cancelColor:'red',
+      success:function(res){
+        console.log(res)
+        if(res.cancle){
+          console.log('用户点击了取消按钮')
+        }
+        if(res.confirm){
+          console.log('用户点击了确定按钮')
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载',
+      mask: true,
+    })
+    setTimeout(function(){
+      //必须手动调用让loading消失
+      wx.hideLoading()
+    }, 2000)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowActionSheeet(){
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      success: function(res){
+        console.log(res)
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+ //分享
+  onShareAppMessage: function(options){
+    return{
+      title: "你好啊，李银河",
+      path: "/pages/about/about"
+    }
   }
 })
